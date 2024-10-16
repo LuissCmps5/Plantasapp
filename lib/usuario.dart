345018287c49fc8a94b7078337b0b3753cf1e5f9
LuissCmps5 <luis.campos@upnay.edu.mx>
2024-10-16 09:54:15 -0700

    agrega el codigo de registro

---
+++ lib/usuario.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'login.dart';  // Asegúrate de importar la página de login
+
+class UsuarioPage extends StatelessWidget {
+  final SupabaseClient _supabase = Supabase.instance.client;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bienvenido'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () async {
+              // Cerrar sesión dentro del onPressed
+              await _supabase.auth.signOut(); 
+              // Redirigir al login
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => LoginPage()),
+              );
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text('Bienvenido Usuario', style: TextStyle(fontSize: 24)),
+      ),
+    );
+  }
+}
